50922298316e03a2da744bf167a6202ab842f0ee
Valeriy <leshikzoloto@gmail.com>
2021-11-05 18:56:26 +0300

    header-footer-desctop and mobile ready about start

--- app/js/main.js
+++ app/js/main.js
@@ -11,4 +11,19 @@ $(function () {
 	$(".burger, .menu__item a").on("click", function () {
 		$(".burger").toggleClass("burger--active");
 	});
+
+	//about-slider
+	$(".about__slider").slick({
+		centerMode: true,
+		dots: true,
+		// centerPadding: "60px",
+		// arrows: false,
+		centerMode: true,
+		variableWidth: true,
+		slidesToShow: 3,
+		prevArrow:
+			'<button type="button" class="slick-prev"><span class="sr-only">Стрелка слайдера влево</span><svg> <use xlink:href="images/sprite.svg#arrow-left"></use></svg></button>',
+		nextArrow:
+			' <button type="button" class="slick-next"><span class="sr-only">Стрелка слайдера вправо</span><svg> <use xlink:href="images/sprite.svg#arrow-right"></use></svg></button>',
+	});
 });
