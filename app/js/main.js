bcb5aa06d4582252b30ea9084f34e57a192638e9
Valeriy <leshikzoloto@gmail.com>
2021-11-10 18:21:07 +0300

    add contacs desctop

--- app/js/main.js
+++ app/js/main.js
@@ -51,4 +51,17 @@ $(function () {
 		nextArrow:
 			' <button type="button" class="slick-next"><span class="sr-only">Стрелка слайдера вправо</span><svg> <use xlink:href="images/sprite.svg#arrow-right"></use></svg></button>',
 	});
+
+	$(".scroll").click(function (event) {
+		console.log(11111);
+		event.preventDefault();
+		$("html").animate({ scrollTop: 0 }, 1500);
+	});
+
+	$(".menu a, header a").on("click", function (e) {
+		e.preventDefault();
+		const id = $(this).attr("href"),
+			top = $(id).offset().top;
+		$("body,html").animate({ scrollTop: top }, 1500);
+	});
 });
